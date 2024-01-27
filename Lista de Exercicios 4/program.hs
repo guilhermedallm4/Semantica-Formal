767bee30ed78f4fd5b9e2ade00e73e9fa1a55566
Guilherme D. Lima <54902373+guilhermedallm4@users.noreply.github.com>
2024-01-27 16:48:14 -0300

    Update program.hs

--- Lista de Exercicios 4/program.hs
+++ Lista de Exercicios 4/program.hs
@@ -1,1 +1,14 @@
+data E = Num Int | Soma E E | Mult E E
+   deriving(Eq,Show)
 
+prog1 :: E
+prog1 = Soma (Num 1) (Mult (Num 2) (Num 3))
+
+prog2 :: E
+prog2 = Soma (Mult (Num 2) (Num 3)) 
+             (Mult (Num 4) (Num 5))
+
+bigStepE :: E -> Int
+bigStepE (Num n) = n
+bigStepE (Soma e1 e2) = bigStepE e1 + bigStepE e2
+bigStepE (Mult e1 e2) = bigStepE e1 * bigStepE e2
